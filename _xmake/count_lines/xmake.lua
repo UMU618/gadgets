c5336138c95b319710527d3efcea2e3620094b9e
UMU618 <umu618@hotmail.com>
2026-02-17 02:14:45 +0800

    * Add directory `umutech`

--- _xmake/count_lines/xmake.lua
+++ _xmake/count_lines/xmake.lua
@@ -3,7 +3,7 @@ add_requires("fmt")
 target("count_lines_fmt")
   set_kind("binary")
   add_defines("USE_FMTLIB")
-  add_files("../../src/count_lines/count_lines.cpp")
+  add_files("../../src/umutech/count_lines/count_lines.cpp")
   add_packages("fmt")
   if is_plat("windows") then
     add_links("shell32")
